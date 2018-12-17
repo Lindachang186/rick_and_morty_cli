3adc71a7f5c32dd6a4ff26c35ab01ee264455285
Linda Chang <lindachang186@gmail.com>
2018-12-17 18:43:35 +0000

    added two methods

--- lib/rick_and_morty_cli/cli.rb
+++ lib/rick_and_morty_cli/cli.rb
@@ -1,10 +1,16 @@
 #My CLI Controller
 require 'pry'
 
+
 class RickAndMortyCli::CLI
 
   def call
+     RickAndMortyCli::Scraper.new
+     puts "Welcome to the Rick And Morty Character Finder"
+  end
 
+  def print_characters
+    
   end
 
 end
--- lib/rick_and_morty_cli/scraper.rb
+++ lib/rick_and_morty_cli/scraper.rb
@@ -5,7 +5,7 @@ class RickAndMortyCli::Scraper
 
   def get_page
     Nokogiri::HTML(open("https://rickandmortyapi.com/"))
-    puts "hi"
+    binding.pry
   end
 
 
