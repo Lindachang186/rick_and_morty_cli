756c2dd125126d1e90fa282a3a1f1d1f4b67664e
Linda Chang <lindachang186@gmail.com>
2018-12-19 16:38:15 +0000

    added a find method in the character.rb

--- lib/rick_and_morty_cli/character.rb
+++ lib/rick_and_morty_cli/character.rb
@@ -18,6 +18,16 @@ class RickAndMortyCli::Character
     @@all
   end
 
-
+  def self.find_by_name
+    @@all.find do |character|
+      if name == character.name
+        puts character.name
+        puts character.id
+        puts character.species
+        puts character.status
+        puts character.episodes
+      end
+    end
+  end
 
 end
--- lib/rick_and_morty_cli/cli.rb
+++ lib/rick_and_morty_cli/cli.rb
@@ -5,22 +5,20 @@ class RickAndMortyCli::Cli
 
   def call
      RickAndMortyCli::Api.new.get_characters
-     binding.pry
      puts "Welcome to the Rick And Morty Character Finder"
      start
   end
 
   def start
-    puts "What Rick And Morty character would you like to find?"
-    input = gets.strip
 
-    if input.is_a?(Integer)
+    if input.is_a?(String)
 
-    elsif input.is_a?(String)
 
     end
-
   end
 
 
+  # Make a list of options
+  # Find Rick and Morty characters by name
+  # Find Random Rick and Morty Character
 end
